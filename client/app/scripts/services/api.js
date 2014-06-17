876279f18c573fe76611ed298c4cdf3902c9a29e
Gorlan Yuen <gorlan@wix.com>
2014-06-17 09:40:41 -0700

    Reset the defaults

--- client/app/scripts/services/api.js
+++ client/app/scripts/services/api.js
@@ -7,7 +7,7 @@ angular.module('sendFiles').factory('api', function ($resource, $wix) {
     noFileText: '',
     emailAddressText: 'Your email address',
     messageText: 'You can add a message to site owner',
-    submitButtonText: 'Submiasdasdsadt'
+    submitButtonText: 'Submit'
   };
 
   var headers = {
