9b6bb3546547f652f753693de2098c62c51debf4
Gorlan Yuen <gorlan@wix.com>
2014-06-25 10:36:02 -0700

    Forgot to add location service as dependency

--- client/app/scripts/services/api.js
+++ client/app/scripts/services/api.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('sendFiles').factory('api', function ($resource, $wix) {
+angular.module('sendFiles').factory('api', function ($resource, $wix, $location) {
   var defaults = {
     headlineText: 'Upload the file and send it to us. We will review it as soon as possible.',
     addButtonText: '+ Add Files',
