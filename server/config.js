7db09b8a66633cf37155343f25b62112db4e2df5
Andrey Elenskiy <andreye@wix.com>
2014-06-25 11:40:19 -0700

    instance as a query param when auth url

--- server/config.js
+++ server/config.js
@@ -1,27 +1,28 @@
 var query = require('pg-query');
 var wix = require('wix');
 
-var MAX_FILE_SIZE = 1024 * 1024 * 1024; // 1GB
-var MAX_UPLOAD_RECOVERS = 10;
+exports.MAX_FILE_SIZE = 1024 * 1024 * 1024; // 1GB
+exports.MAX_UPLOAD_RECOVERS = 10;
+exports.EXPIRE_TIME = 1000 * 60 * 60 * 23; // 23 hours in miliseconds
 
-var TMP_DIR = './tmp/';
-var CLIENT_APP_DIR = '../../client/app';
+exports.TMP_DIR = './tmp/';
+exports.CLIENT_APP_DIR = '../../client/app';
 
 
-var googleKeys = {
+exports.googleKeys = {
   clientId: process.env.GOOGLE_CLIENT_ID || require('./connect-keys/client-id.json').web.client_id,
   redirectUri: process.env.GOOGLE_REDIRECT_URI || require('./connect-keys/client-id.json').web.redirect_uris[0],
   clientSecret: process.env.GOOGLE_CLIENT_SECRET || require('./connect-keys/client-id.json').web.client_secret
 };
 
 
-var wixKeys = {
+exports.wixKeys = {
   appKey : process.env.WIX_APP_KEY || require('./connect-keys/wix-key.json').appKey,
   secretKey : process.env.WIX_SECRET_KEY || require('./connect-keys/wix-key.json').secretKey
 };
 
 
-var mailgunKeys = {
+exports.mailgunKeys = {
   username: process.env.MAILGUN_SMTP_LOGIN || require('./connect-keys/mailgun-key.json').username,
   pass: process.env.MAILGUN_SMTP_PASSWORD || require('./connect-keys/mailgun-key.json').pass,
   apiKey: process.env.MAILGUN_API_KEY || require('./connect-keys/mailgun-key.json').apiKey
@@ -32,17 +33,6 @@ query.connectionParameters = process.env.DATABASE_URL || require('./connect-keys
 wix.secret(wixKeys.secretKey);
 
 module.exports = {
-  // constants
-  MAX_FILE_SIZE: MAX_FILE_SIZE,
-  MAX_UPLOAD_RECOVERS: MAX_UPLOAD_RECOVERS,
-  // directories
-  TMP_DIR: TMP_DIR,
-  CLIENT_APP_DIR: CLIENT_APP_DIR,
-  // api keys
-  wixKeys: wixKeys,
-  googleKeys: googleKeys,
-  mailgunKeys: mailgunKeys,
-  // packages
   query: query,
   wix: wix,
 };
