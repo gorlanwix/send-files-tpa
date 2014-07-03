0b4bb88fc418f150535aa3f86627a9b600737031
Jeffrey <jeffrey@wix.com>
2014-07-03 15:23:00 -0700

    Added commenting to all directives

--- client/app/scripts/directives/adjustBlockSize.js
+++ client/app/scripts/directives/adjustBlockSize.js
@@ -1,5 +1,11 @@
 'use strict';
-
+/**
+ * This directive binds event listeners to the file block and the message
+ * box that listen for when the user interacts with those blocks. When the user
+ * scrolls in the files block - and they've uploaded at least 7 files - the
+ * file block will grow and the message box will shrink. The opposite occurs
+ * when the user begins typing in the message box.
+ */
 angular.module('sendFiles')
   .directive('adjustBlockSize', function () {
         return {
@@ -21,7 +27,7 @@ angular.module('sendFiles')
             };
 
             angular.element(filesBlock).bind('scroll', function () {
-              if (scope.totalFilesAdded > 7 && (!scope.focusedTextarea || scope.onFiles)) {
+              if (scope.totalFilesAdded >= 7 && (!scope.focusedTextarea || scope.onFiles)) {
                 angular.element(filesBlock).addClass('files-enlarge');
                 angular.element(textareaBlock).addClass('message-box-shrink');
               }
--- client/app/scripts/directives/dropping.js
+++ client/app/scripts/directives/dropping.js
@@ -1,5 +1,9 @@
 'use strict';
-
+/**
+ * This directive adds event listeners to make the "drop files here" element
+ * appear when the user drags a file anywhere over the widget. The element
+ * disappears once the files are dropped or dragged out of the widget.
+ */
 angular.module('sendFiles')
   .directive('dropping', function() {
         return {
